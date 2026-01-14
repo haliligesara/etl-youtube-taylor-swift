6195e49a02f342894ed741f67a3bbbaf592cfb58
haliligesara <gesara.halili@gmail.com>
2026-01-14 13:14:24 +0100

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,6 @@ from airflow.models import Variable, Connection, DagBag
 def api_key():
     with mock.patch.dict("os.environ", {"AIRFLOW_VAR_API_KEY": "MOCK_KEY123"}):
         yield Variable.get("API_KEY")
-
-@pytest.fixture
-def mock_postgres_conn_vars():
-    conn = Connection(
-        login="mock_username",
-        password="mock_password",
-        host="mock_host",
-        port="1234",
-        schema="mock_db_name",
-    )
-    conn_uri = conn.get_uri()
-
-    with mock.patch.dict("os.environ", {"AIRFLOW_CONN_POSTGRES_DB_YT_ELT": conn_uri}):
-        yield Connection.get_connection_from_secrets(conn_id="POSTGRES_DB_YT_ELT")
     
 @pytest.fixture
 def dagbag():
